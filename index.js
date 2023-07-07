0a4e5acd5481d529f748d50c21f2df1e559686e3
zwithe <zwithe133@gmail.com>
2023-07-07 11:20:07 -0400

    fixed repeating sections

--- index.js
+++ index.js
@@ -2,22 +2,7 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 
-require('dotenv').config()
-const express = require('express')
-const app = express()
-
 app.use('/places', require('./controllers/places'))
-
-app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
-
-app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
-})
-
-app.listen(process.env.PORT)
-
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
