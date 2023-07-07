67e007b1cbc2906267384ada41631a6dcb5acb1a
zwithe <zwithe133@gmail.com>
2023-07-07 11:16:48 -0400

    added places controller

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.use('/places', require('./controllers/places'))
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+app.listen(process.env.PORT)
+
+
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
