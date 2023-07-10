3034eb8473ff899a248c7d308c79c7664bafc180
zwithe <zwithe133@gmail.com>
2023-07-10 12:07:16 -0400

    added home page

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-require("dotenv").config()
-const express = require("express")
+require('dotenv').config()
+const express = require('express')
 const app = express()
+
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.render('home')
 })
+  
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page not found</h1>')
+  res.send('404 page')
 })
 
 app.listen(process.env.PORT)
