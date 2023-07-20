944dc483fbaa079e61f98d144320f59d8b4f3e70
zwithe <zwithe133@gmail.com>
2023-07-20 17:27:20 -0400

    added seeder updated image name

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('DB connected'))
