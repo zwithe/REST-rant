4c5d0f86e6e3340665d64a354e4d376b3f92f154
zwithe <zwithe133@gmail.com>
2023-07-28 15:49:21 -0400

    added stars for rating

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,6 +7,29 @@ function show (data) {
       No comments yet!
     </h3>
   )
+  let rating = (
+    <h3 className="inactive">
+      Not yet rated
+    </h3>
+  )
+  if (data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars
+    }, 0)
+    let averageRating = Math.round(sumRatings / data.place.comments.length)
+    let stars = ''
+    for (let i = 0; i < averageRating; i++) {
+      stars += '⭐'
+    }
+    rating = (
+      <h3>
+        {stars} stars
+      </h3>
+    )
+    
+  }
+  
+  
   if (data.place.comments.length) {
     comments = data.place.comments.map(c => {
       return (
@@ -45,17 +68,15 @@ function show (data) {
                 <h2>
                   Rating
                 </h2>
-                <p>
-                  No rating yet
-                </p>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-              Edit
-            </a>
-            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-              <button type="submit" className="btn btn-danger">
-                Delete
-              </button>
-            </form>     
+                {rating}    
+                <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                  Edit
+                </a>
+                <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                  <button type="submit" className="btn btn-danger">
+                    Delete
+                  </button>
+                </form>     
               </section>
             </div>
             <hr/>
